398b141b6555c341b0343308a6a05195ef98d215
PauloSoVieira <paulo92vieira@gmail.com>
2024-12-03 09:13:52 +0000

    slide nao está funcional

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 100000000000000000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
