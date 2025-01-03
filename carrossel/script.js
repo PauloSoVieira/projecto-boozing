de9956299ba1e6b39183cc7d57e3a04fa67249fa
Paulo Vieira <paulo92vieira@gmail.com>
2025-01-03 21:18:20 +0000

    fix : Header and carrossel home

--- carrossel/script.js
+++ carrossel/script.js
@@ -1,29 +1,31 @@
-const track = document.querySelector('.carousel-track');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const track = document.querySelector(".carousel-track");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 
 const itemsToShow = 3;
-const totalItems = document.querySelectorAll('.city-card').length;
-const itemWidth = document.querySelector('.city-card').offsetWidth + 30; // Inclui a margem
+const totalItems = document.querySelectorAll(".city-card").length;
+const itemWidth = document.querySelector(".city-card").offsetWidth + 50; // Inclui a margem
 let currentIndex = 0;
 
 function updateCarousel() {
   // Atualiza a posição do carrossel
-  track.style.transform = `translateX(-${currentIndex * itemsToShow * itemWidth}px)`;
+  track.style.transform = `translateX(-${
+    currentIndex * itemsToShow * itemWidth
+  }px)`;
 
   // Habilita/desabilita botões
   prevBtn.disabled = currentIndex === 0;
   nextBtn.disabled = (currentIndex + 1) * itemsToShow >= totalItems;
 }
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
   if (currentIndex > 0) {
     currentIndex--;
     updateCarousel();
   }
 });
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener("click", () => {
   if ((currentIndex + 1) * itemsToShow < totalItems) {
     currentIndex++;
     updateCarousel();
